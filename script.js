8d08f0f9934fce373c2a496bf4520f0e283c1281
Pelé <rian.amaro@escola.pr.gov.br>
2026-04-27 14:41:54 +0000

    salvei 27/04

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const botao = document.getElementById("meuBotao");
+
+    botao.addEventListener("click", function() {
+        alert("o botão foi clicado");
+    });
+});
